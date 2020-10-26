681061c583ef2affb1decf32c3d6200b7904115b
Anton Finnson <afinnson@kth.se>
2020-10-26 11:33:56 +0100

    fixed bug with default value not being used

--- lab2/2-1_myexp/main.cpp
+++ lab2/2-1_myexp/main.cpp
@@ -7,7 +7,9 @@ int main(int argc, char const *argv[])
 {
   double x = atof(argv[1]);
   double tol = atof(argv[2]);
-  double res = myexp(x, tol);
+  double res; 
+  if (tol == 0) res = myexp(x); // use default tol if no tol given, or tol == 0 (impossible to achieve)
+  else res = myexp(x, tol);
   std::cout << "exp(x) = " << res << std::endl;
   std::cout << "error = " << abs(exp(x) - res) << std::endl;
   return 0;
--- lab2/2-1_myexp/myexp.cpp
+++ lab2/2-1_myexp/myexp.cpp
@@ -4,13 +4,14 @@
 
 double myexp(const double &x, const double tol) {
   if (x < 0) return 1/myexp(-x, tol);
-  int N = ceil(x); // make sure N >= x
+  int N = 0;
   double err = (x / 2) * (1 + (x * (x + 1)) / (x + 3));
   while (err > tol) {
     ++N;
     err *= (x/(N+2));
   }
-  std::cout << "N: " << N << std::endl;
+  if (N < x) N = ceil(x); // make sure N >= x
+  std::cout << "Summing up to n = " << N << std::endl;
   double res = 1;
   for (int i = N; i > 0; --i) {
     res = 1 + res * x / i;
