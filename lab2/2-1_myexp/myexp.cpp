22ab75798a5c249de4f5de372a8c5494e10c0074
Anton Finnson <afinnson@kth.se>
2020-10-26 11:51:24 +0100

    fixed approximation of N again

--- lab2/2-1_myexp/myexp.cpp
+++ lab2/2-1_myexp/myexp.cpp
@@ -5,10 +5,10 @@
 double myexp(const double &x, const double tol) {
   if (x < 0) return 1/myexp(-x, tol);
   int N = 0;
-  double err = (x / 2) * (1 + (x * (x + 1)) / (x + 3));
+  double err = (x / 2) * (1 + (x * (x + 1)) / (x + 2));
   while (err > tol) {
     ++N;
-    err *= (x/(N+2));
+    err *= (x/(N+1));
   }
   if (N < x) N = ceil(x); // make sure N >= x
   std::cout << "Summing up to n = " << N << std::endl;
