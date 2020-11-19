cb78761b1593d304453565b231441da0899145ba
jonasnylund <35763296+jonasnylund@users.noreply.github.com>
2020-11-19 16:14:06 +0100

    bättre plot

--- lab3/plot.py
+++ lab3/plot.py
@@ -15,6 +15,15 @@ for i in range((width+1)*(height+1)):
 	x.append(np.fromfile(file, dtype=float, count=1));
 	y.append(np.fromfile(file, dtype=float, count=1));
 
-
 plt.scatter(x, y);
+
+for i in range(width*height):
+	plt.plot([x[i+1], x[i], x[i+width+1]], [y[i+1], y[i], y[i+width+1]])
+
+for i in range(width):
+	plt.plot([x[(width+1)*(height+1)-width - 1 + i], x[(width+1)*(height+1)-width + i]], [y[(width+1)*(height+1)-width -1 + i], y[(width+1)*(height+1)-width + i]])
+
+for i in range(height):
+	plt.plot([x[(i+1)*(width+1)-1], x[(i+2)*(width+1)-1]], [x[(i+1)*(width+1)-1], y[(i+2)*(width+1)-1]]);
+
 plt.show();
